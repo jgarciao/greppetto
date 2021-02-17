9d5270c28715cadbf43f9e4ed39e43b3f80ca41c
Jorge García Oncins <jgarciao@gmail.com>
2021-02-17 11:21:09 +0100

    Add unit tests for formatters

--- test_greppetto.py
+++ test_greppetto.py
@@ -66,6 +66,95 @@ class Test(TestCase):
         # THEN returned matches are the expected
         self.assertEqual(match_intervals, expected_intervals, "Should be " + str(expected_intervals))
 
+    def test_default_matched_line_formatter(self):
+        """
+        Test DefaultMatchedLineFormatter with an input_line that contains the pattern
+        """
+
+        # GIVEN input_line with one occurrence of the pattern
+        filename = "myfilename"
+        line_number = "1"
+        input_line = "text mypattern text mypattern"
+        match_intervals = [(5, 14), (20, 29)]
+        expected_formatted_line = "myfilename:1:text mypattern text mypattern"
+
+        # WHEN find pattern in input_line
+        factory = greppetto.MatchedLineFormatterFactory()
+        factory.register_format(greppetto.MatchedLineFormat.DEFAULT, greppetto.DefaultMatchedLineFormatter)
+        line_formatter = factory.get_formatter(greppetto.MatchedLineFormat.DEFAULT)
+
+        formatted_string = line_formatter.format(filename, line_number, input_line, match_intervals)
+
+        # THEN the formatted string has the expected format
+        self.assertEqual(formatted_string, expected_formatted_line, "Should be " + str(expected_formatted_line))
+
+    def test_color_matched_line_formatter(self):
+        """
+        Test ColorMatchedLineFormatter with an input_line that contains the pattern
+        """
+
+        # GIVEN input_line with one occurrence of the pattern
+        filename = "myfilename"
+        line_number = "1"
+        input_line = "text mypattern text mypattern"
+        match_intervals = [(5, 14), (20, 29)]
+        expected_formatted_line = "myfilename:1:text [95mmypattern[0m text [95mmypattern[0m"
+
+        # WHEN find pattern in input_line
+        factory = greppetto.MatchedLineFormatterFactory()
+        factory.register_format(greppetto.MatchedLineFormat.COLOR, greppetto.ColorMatchedLineFormatter)
+        line_formatter = factory.get_formatter(greppetto.MatchedLineFormat.COLOR)
+
+        formatted_string = line_formatter.format(filename, line_number, input_line, match_intervals)
+
+        # THEN the formatted string has the expected format
+        self.assertEqual(formatted_string, expected_formatted_line, "Should be " + str(expected_formatted_line))
+
+    def test_underscore_matched_line_formatter(self):
+        """
+        Test UnderscoreMatchedLineFormatter with an input_line that contains the pattern
+        """
+
+        # GIVEN input_line with one occurrence of the pattern
+        filename = "myfilename"
+        line_number = "1"
+        input_line = "text mypattern text mypattern"
+        match_intervals = [(5, 14), (20, 29)]
+        expected_formatted_line = "myfilename:1:text mypattern text mypattern\n" + \
+                                  "                  ^^^^^^^^^      ^^^^^^^^^"
+
+        # WHEN find pattern in input_line
+        factory = greppetto.MatchedLineFormatterFactory()
+        factory.register_format(greppetto.MatchedLineFormat.UNDERSCORE, greppetto.UnderscoreMatchedLineFormatter)
+        line_formatter = factory.get_formatter(greppetto.MatchedLineFormat.UNDERSCORE)
+
+        formatted_string = line_formatter.format(filename, line_number, input_line, match_intervals)
+
+        # THEN the formatted string has the expected format
+        self.assertEqual(formatted_string, expected_formatted_line, "Should be " + str(expected_formatted_line))
+
+    def test_machine_matched_line_formatter(self):
+        """
+        Test MachineReadableMatchedLineFormatter with an input_line that contains the pattern
+        """
+
+        # GIVEN input_line with one occurrence of the pattern
+        filename = "myfilename"
+        line_number = "1"
+        input_line = "text mypattern text mypattern"
+        match_intervals = [(5, 14), (20, 29)]
+        expected_formatted_line = "myfilename:1:5:mypattern\nmyfilename:1:20:mypattern"
+
+        # WHEN find pattern in input_line
+        factory = greppetto.MatchedLineFormatterFactory()
+        factory.register_format(greppetto.MatchedLineFormat.MACHINE, greppetto.MachineReadableMatchedLineFormatter)
+        line_formatter = factory.get_formatter(greppetto.MatchedLineFormat.MACHINE)
+
+        formatted_string = line_formatter.format(filename, line_number, input_line, match_intervals)
+
+        # THEN the formatted string has the expected format
+        self.assertEqual(formatted_string, expected_formatted_line, "Should be " + str(expected_formatted_line))
+
 
 if __name__ == '__main__':
     unittest.main()
